05ab5207745c939ba02f4904d2247eeba8f96fb3
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-25 09:55:33 -0400

    tudo ok

--- Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 2 - O Ambiente Web Cliente X Servidor e as Tecnologias/M1 - O ambiente web/pratica01/server.php
+++ Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 2 - O Ambiente Web Cliente X Servidor e as Tecnologias/M1 - O ambiente web/pratica01/server.php
@@ -6,10 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="server.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
+    Welcome <?php echo $_POST["name"]; ?>
+    Your email address is: <?php echo $_POST["email"]; ?>
 </body>
 </html>
