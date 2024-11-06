e1b7e3789bc7a4f096d0f5ef956c5eb5ee0a5ae9
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-11-06 10:43:02 -0400

    ex04 ok

---
+++ Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 5 - Linguagem JavaScript/M2/ex003/ex04/interaction05.js
@@ -0,0 +1,5 @@
+var frutas = ['Laranja', 'Uva', 'Pera']
+/*Imprimindo na caixa de diálogo o conteúdo do array 'frutas' utilizando o laço 'for/in'*/
+for (var x in frutas) {
+    console.log('Nome da Fruta contida no Array: ' + frutas[x])
+}
