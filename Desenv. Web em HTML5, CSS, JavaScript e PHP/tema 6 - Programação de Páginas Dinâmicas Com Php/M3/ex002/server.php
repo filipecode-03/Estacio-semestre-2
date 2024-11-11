9dcf5779a3eaecccaea85c41570a2b2b64b9875f
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-11-11 11:51:18 -0400

    tema 6 finalizado

---
+++ Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 6 - Programação de Páginas Dinâmicas Com Php/M3/ex002/server.php
@@ -0,0 +1,15 @@
+<?php 
+    $num1 = 10;
+    $num2 = 15;
+
+    function soma($numero1, $numero2) {
+        return $numero1 + $numero2;
+    }
+
+    function imprimir_resultado($numero) {
+        echo "O resultado da operação é igual a: " . $numero;
+    }
+
+    $num3 = soma($num1, $num2);
+    imprimir_resultado($num3);
+?>
