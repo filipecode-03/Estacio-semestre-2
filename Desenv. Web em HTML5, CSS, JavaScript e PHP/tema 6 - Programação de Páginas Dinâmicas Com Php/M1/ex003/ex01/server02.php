5e84be02068752bcc3b4fa551c7ea52ec4abf141
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-11-11 09:02:25 -0400

    ex003 ok

---
+++ Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 6 - Programação de Páginas Dinâmicas Com Php/M1/ex003/ex01/server02.php
@@ -0,0 +1,13 @@
+<?php 
+    //Comentários de uma linha são realizados com a utilização de barras duplas
+
+    /*
+    Cada comando deve ser terminado com ;
+    Abaixo temos alguns exemplos de término de comandos
+    */
+
+    echo "Olá, mundo\n";
+    $var1 = 2 + 2;
+
+    echo "O resultado da soma é igual a " . $var1;
+?>
---
+++ Desenv. Web em HTML5, CSS, JavaScript e PHP/tema 6 - Programação de Páginas Dinâmicas Com Php/M1/ex003/ex01/server03.php
@@ -0,0 +1,5 @@
+<?php 
+    $_nomeCurso = "Programação de Páginas Dinâmicas com PHP";
+    $ano_criacao = 1994;
+    $flagLinguagemScript = true;
+?>
